cd26fc7dff3b6d018f3070d99a2c5ddb039eff85
goatastronaut0212 <goatastronaut0212@outlook.com>
2025-03-21 17:40:30 +0700

    Update start.sh and ci flow

--- start.sh
+++ start.sh
@@ -8,15 +8,6 @@ set -e
 echo "Running migration"
 goose up
 
-# Run test
-go test -v cmd/services/database.go cmd/services/database_test.go
-go test -v cmd/services/auth.go cmd/services/auth_test.go
-go test -v cmd/services/database.go cmd/services/blogtag.go cmd/services/blogtag_test.go
-go test -v cmd/services/database.go cmd/services/blogtag.go cmd/services/blogpost.go cmd/services/blogpost_test.go
-go test -v cmd/routes/auth.go cmd/routes/auth_test.go
-go test -v cmd/routes/auth.go cmd/routes/blogtag.go cmd/routes/blogtag_test.go
-go test -v cmd/routes/auth.go cmd/routes/blogpost.go cmd/routes/blogpost_test.go
-
 # Start server
 echo "Starting server..."
 exec ./api-chi
